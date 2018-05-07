90b9ca55d7bdd044833f246f8f9c9a6d2184a4ce
EaglesRoboticsTeam <38738763+EaglesRoboticsTeam@users.noreply.github.com>
2018-05-07 17:47:07 +0000

    Update seguidor_color.py

--- seguidor_color.py
+++ seguidor_color.py
@@ -1,10 +1,10 @@
-#wyznawca linii dla rapbarry i brick pi z brazylijskiej Brazylii
-#versão 0.2.5
+#Wyznawca linii dla rapbarry i brick pi z brazylijskiej Brazylii
+#Line follower z tylko jednym czujnikiem koloru
+#Versão 0.3
 import time
 import brickpi3
 
 BP = brickpi3.BrickPi3()
-
 BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.EV3_COLOR_COLOR)
 
 color = ["none", "Black", "Blue", "Green", "Yellow", "Red", "White", "Brown"]
@@ -12,16 +12,14 @@ color = ["none", "Black", "Blue", "Green", "Yellow", "Red", "White", "Brown"]
 try:
     while True:
         try:
-            value = BP.get_sensor(BP.PORT_1)#value recebe valores do sensor da porta 1
+            value = BP.get_sensor(BP.PORT_1)#value recebe valores do sensor da porta 1    
             
             if color[value] == "Black":
-                time.sleep(0.001)
-                print("Motor A 100")
-                
+                time.sleep(0)
+                print("Motor A 100")        
             else:
-                time.sleep(0.001)
-                print("Motor B 100")
-                
+                time.sleep(0)
+                print("Motor B 100")               
         except brickpi3.SensorError as error: #CASO O SENSOR DEMORE PARA INICIALIZAR O SENSOR ERRO SERVE PARA 
             print(error)
 except KeyboardInterrupt: #FECHANDO O PROGRAMA
